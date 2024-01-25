f27bcbaa70ff0a3970fcbcaef1b14054409a2b39
Goran Stanic <goran.stanic@dkfz-heidelberg.de>
2024-01-25 11:30:04 +0100

    Added black

--- src/tests/test_transform_unittest.py
+++ src/tests/test_transform_unittest.py
@@ -3,6 +3,7 @@ import numpy as np
 import transform as tf
 
 
+
 class test_area_circ(unittest.TestCase):
     def test_area_circ(self):
         """Test the area values against a reference for r >= 0."""
@@ -10,6 +11,8 @@ class test_area_circ(unittest.TestCase):
         self.assertEqual(tf.area_circ(0), 0)
         self.assertEqual(tf.area_circ(2.1), np.pi * 2.1**2)
 
+
     def test_values(self):
         """Make sure value errors are recognized for area_circ."""
         self.assertRaises(ValueError, tf.area_circ, -5)
+
